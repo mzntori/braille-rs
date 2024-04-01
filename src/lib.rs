d3b86206e730dd7f544b29780fab5e2cb0d9bb61
mzntori <mzntori@proton.me>
2024-04-01 22:48:20 +0200

    add README contents to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # braille-rs
+//!
+//! A Rust crate that aims to simplify working with braille characters.
+//! If you actually want to use braille art in a project i recommend the [rsille](https://crates.io/crates/rsille) crate, since it will most likely be better maintained and has a lot more features.
+
 pub mod braille_char;
 pub mod canvas;
 pub mod error;
